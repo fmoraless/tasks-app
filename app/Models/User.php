d6f6a8038025e8e0d02d80a0a9450f2fa87b9999
Francisco Morales Sánchez <fcomorales.sanchez@gmail.com>
2022-07-24 02:07:08 -0400

    add uuid for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasUuid;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUuid;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +24,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +48,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public $resourceType = 'managers';
+
+    public function getResourceType(): string
+    {
+        return 'managers';
+    }
+
 }
